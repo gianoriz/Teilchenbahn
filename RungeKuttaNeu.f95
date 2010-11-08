f3f3436aa71c537d6ec94a43ac517e5d050cad1b
Giano Rizzo <rizzo@moe@geo.uni-koeln.de>
2010-11-08 15:44:04 +0100

    Jonas Bemerkungen bearbeitet

--- RungeKuttaNeu.f95
+++ RungeKuttaNeu.f95
@@ -1,7 +1,7 @@
 program TeilchenTrajektorie
 
   !PROGRAMM: RUNGE-KUTTA-4TER-ORDNUNG (MIT DEN RICHTIGEN BEWEGUNGSGLEICHUNGEN)
-  !Jonas schau dir bitte meinen Rungekuttasolver an und vergleiche dies mit dem online numerical recepies!!!
+
   implicit none   
 
   !DEFINITION DER VARIABLEN:
@@ -9,34 +9,20 @@ program TeilchenTrajektorie
   double precision xr, v_xr, a_xr       !x-Koord. & Geschw. im Rhea-Ruhesystem  
 
   !***********************************
-  ! wie gross ist double precision? Am besten zur Erinnerung in Kommentar schreiben.
+  ! wie gross ist double precision? Am besten zur Erinnerung in Kommentar schreiben. KEINE AHNUNG, WO SEHE ICH NACH???
   !***********************************
-
-
 
   double precision yr, v_yr, a_yr       !y-Koord. & Geschw. im Rhea-Ruhesystem
   double precision zr, v_zr, a_zr       !z-Koord. & Geschw. im Rhea-Ruhesystem 
   double precision t, dt                !Flugzeit & Zeitintervall
   double precision v_0                  !Anfangsgeschw.              
-
-  double precision alpha                !Abwurfwinkel                    
-  !***********************************
-  ! Abwurfwinkel??? Hier stehen jede Menge Variablen die kein Mensch braucht. Wie soll man da noch durchblicken?
-  !***********************************
-
+  double precision alpha                !Abwurfwinkel wird spaeter benutzt                    
   double precision omegaz               !Winkelgeschw. in z-Richtung 
-  double precision gamma                !Gravitationskonstante       
-  double precision Ms, Mr               !Masse Saturn, Masse Rhea    
   double precision v_r                  !Geschw. von Rhea um Saturn  
-  double precision Mj, Mk               !Hilfsgroessen: Mj = 3 bzw. 6 & Mk = 2 bzw. 1
-  !***********************************
-  ! ^^^^ versteh ich nicht
-  !***********************************
-
-
+  double precision L1 
   double precision A                    !Hilfsgr.: eliminiert nichtvorhandene Matrixelemente 
   !***********************************
-  ! verstehe ich nicht. sieht mir sehr sehr seltsam aus. Flicken wir spaeter. 
+  ! verstehe ich nicht. sieht mir sehr sehr seltsam aus. Flicken wir spaeter. OK, DAS IST WIRKLICH EIN PROBLEM ERKLAERE ICH SPAETER 
   !***********************************
 
 
@@ -44,60 +30,46 @@ program TeilchenTrajektorie
   double precision, dimension(6) :: k2
   double precision, dimension(6) :: k3
   double precision, dimension(6) :: k4
-
-
   double precision, dimension(9) :: W   !W(i)=(W(1)=x,...,W(4)=v_x,...,W(7)=a_x) Vektor   
-  real :: Pi = 3.14159265358979323846   !Ludolphsche Zahl 
+
+
+  !Konstanten:
+  real, parameter:: PI = 3.14159265358979323846 !Ludolphsche Zahl
+  real, parameter:: gamma = 6.67428*10**(-11.0) ![m^3/(kg*s^2)]   Gravitationskonstante        (Wiki)   
+  real, parameter:: Ms = 5.685*10**(26.0)       ![kg] Masse Saturn                             (Wiki)
+  real, parameter:: Mr = 0.000023166*10**(26.0) ![kg] Masse Rhea                               (Wiki)
+  real, parameter:: R = 764000.0                ![m]  Rhearadius                               (Wiki) 
+  real, parameter:: Rs = 57316000               ![m]  Saturnradius=(120536000.0+108728000.0)/4 (Wiki)  
+  real, parameter:: d = 527040000.0             ![m]  Entf. Rhea zu Saturn                     (Wiki) 
+
+  integer*8 :: q, n, i, j                       !Laufvariablen
+
 
   !***********************************
-  ! wie gross ist Datentyp REAL? Genau nachpruefen. Ist Pi so OK???
+  ! wie gross ist Datentyp REAL? Genau nachpruefen. Ist Pi so OK??? WEISS ICH NICHT!
   !***********************************
 
 
-  double precision d, L1, L2            !Abstand Saturn-Rhea;L1,L2=Lagrangepunkte
-  double precision S                    !Gemeinsamer Schwerpunkt
-  double precision Rs                   !Saturnradius
-  double precision R                    !Rhearadius              
-  integer :: q, n, i, j                 !Laufvariablen
+ 
+
 
 
   !#####################################
-  !Koordinatenursprung liegt bei Rhea
+  !Koordinatenursprung liegt in Rhea
   !#####################################
 
-  !***********************************
-  ! Konstanten in Fortran so: 
-  ! http://de.wikibooks.org/wiki/Fortran:_Fortran_95:_Datentypen#Benannte_Konstanten 
-  ! Bitte alle richtig setzen. 
-  ! also z.B.
-  ! real :: Pi, parameter = 3.1415   !Ludolphsche Zahl 
-  !***********************************
-
-  !SETZE KONSTANTEN & PARAMETER:
+                              
+  omegaz = 0.0!sqrt((gamma * Ms)/(d**3.0))            ![1/s] Winkelgeschw.      
+  L1 = d - (d/( sqrt(Mr/Ms)+1.0))                     ![m]                       
 
   !***********************************
-  ! in Fortran: KONSTANTEN = PARAMETER  => Missverstaendlich
-  ! Variablen sauber von Konstanten trennen!
-  !***********************************
-
-!**** hoch Drei nicht so schreiben  gamma = 6.67428e-11                               ![m³/kg*s²]                 (Wiki)
-  gamma = 6.67428e-11                               ![m^3 / (kg*s^2)]                 (Wiki)
-  d = 527040000.0                                   ![m]   Entf. Rhea zu Saturn (Wiki) 
-  Ms = 5.685e26                                     ![kg]  Masse Saturn         (Wiki)
-  Mr = 0.000023166e26                               ![kg]  Masse Rhea 2.3166e21 (berechnet)
-  omegaz = 0.0!sqrt((gamma * Ms)/(d**3))            ![1/s] Winkelgeschw.        (berechnet) 
-  L1 = d - (d/( sqrt(Mr/Ms)+1))                     ![m]                        (berechnet)
-  R = 764000.0                                      ![m] Rhearadius             (Wiki) 
-
-  Rs = (120536000.0 + 108728000.0)/4                ![m] Saturnradius
-  !***********************************
-  ! Ausrechnen, einsetzen!
+  ! Ausrechnen, einsetzen! NEIN WIRD ZU UNGENAU
   !***********************************
 
 
 
-  dt   = 7.0                                        ![s]
-  n    = 70000                                      ![Anzahl der Iterationen]
+  dt   = 10.0                                        ![s]
+  n    = 1000.0                                       ![Anzahl der Iterationen]
 
 
 
@@ -125,21 +97,21 @@ program TeilchenTrajektorie
 
   do q = 0, n !Grosse do-Zeitschleife!Muss ueberhaupt eine do Zeitschleife rein?
 
-  !***********************************
-  ! Stufe 3
-  ! Keine Ahnung... Das sollte der Programmierer wissen... Unbedingt erst ein Konzept auf Papier machen.
-  ! Aus dem Bauch raus vermute ich aber mal dass Du diese Schleife brauchst. Aendere Dein Programm aber 
-  ! besser bald mal um, so dass Abbruch, wenn Delta(Ergebnis) < Zielgenauigkeit
-  !***********************************
+     !***********************************
+     ! Stufe 3
+     ! Keine Ahnung... Das sollte der Programmierer wissen... Unbedingt erst ein Konzept auf Papier machen.
+     ! Aus dem Bauch raus vermute ich aber mal dass Du diese Schleife brauchst. Aendere Dein Programm aber 
+     ! besser bald mal um, so dass Abbruch, wenn Delta(Ergebnis) < Zielgenauigkeit  
+     !***********************************
 
 
      do  i = 1,6
         k1(i) = dt * W(i+3)
      end do
 
-  !***********************************
-  ! Den W's widmen wir uns, in Stufe 2
-  !***********************************
+     !***********************************
+     ! Den W's widmen wir uns, in Stufe 2 WAS MEINST DU MIT STUFE 2?
+     !***********************************
 
      W(7) = -(gamma * Mr * W(1))/(W(1)**2 + W(2)**2 + W(3)**2)**(1.5) & 
           -(gamma * Ms * W(1))/(W(1)**2 + (W(2) - d)**2 + W(3)**2)**(1.5) & !Teilchen in Rhea Test
@@ -152,13 +124,8 @@ program TeilchenTrajektorie
 
 
 
-     do  i = 1,6
-        if ((i > 3) .or. (j == 1)) then            !  Beseitigt nichtvorhandene Matrixelem.                   
-
-  !***********************************
-  ! Was ist j ?? j wurde nie gesetzt. Damit kann das Programm hier machen
-  ! was es will! Unbedingt Variablenamen nehmen, die man lesen kann. Dann passiert sowas nicht.
-  !***********************************
+     do  i = 1,6 
+        if (i > 3) then                            !  Beseitigt nichtvorhandene Matrixelem.                   
            A = 0.0                                 !  K(1,7),K(1,8),...,K(1,10)                              
         else                                       
            A = 1.0
@@ -214,14 +181,12 @@ program TeilchenTrajektorie
 
      !write(*,*) W(2)/R, W(1)/R!W(2)=y-Koordinate in Rhearadien, W(1)=x-Koordinate in Rhearadien
      !write(*,*) W(2)/R, W(5) * (3600/1000) 
-      write(*,*) t/3600, W(2)-L1!/R
+     write(*,*) t/3600, W(2)-L1!/R
 
-  !***********************************
-  ! was soll da Ausgegeben werden? Spalte 2 ist hier immer 0
-  ! lass t erstmal in Sekunden. Umrechnen erst wenn alles laeuft
-  !***********************************
-
-     !write(*,*) t, W(2)-L1!/R
+     !***********************************
+     ! was soll da Ausgegeben werden? Spalte 2 ist hier immer 0 DIE SPALTE SOLL AUCH NULL SEIN ICH WILL NUR DIE AENDERUNG SEHEN WILL!
+     ! lass t erstmal in Sekunden. Umrechnen erst wenn alles laeuft
+     !***********************************
 
 
   end do !Ende der grossen do-Zeitschleife 
