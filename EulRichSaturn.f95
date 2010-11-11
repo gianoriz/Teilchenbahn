9a44f9b85e3e08845f78af4e5e66d265f63db2b8
Giano Rizzo <rizzo@moe@geo.uni-koeln.de>
2010-11-11 22:17:07 +0100

    ich habe wieder etwas weitergearbeitet

--- EulRichSaturn.f95
+++ EulRichSaturn.f95
@@ -20,7 +20,7 @@ program SaturnRheaTeilchen
   double precision L1                       !Lagrangepunkt
   double precision omegaz                   !Winkelgeschw. in z-Richt.
   integer*8 :: i, n                         !Laufvariablen
-
+  integer :: schrittzaehler                 ! Fuer die Hauptschleife mit dt
 
   !Werte einsetzten: 
   gamma = 6.67428e-11                       ![m³/kg*s²]                 (Wiki)  
@@ -56,81 +56,95 @@ program SaturnRheaTeilchen
 
   dt = 65.0    ![s]       
   n = 100000    !Anzahl der Iterationen
+  !  Schrittweite: 
+  do schrittzaehler = 1, 10
 
-
-  Zeitschleife : do i = 0, n                    
-
-
-     a_x = -(gamma * Mr * x)/(x**2 + y**2 + z**2)**(1.5) & 
-           -(gamma * Ms * x)/(x**2 + (y - d)**2 + z**2)**(1.5) & 
-           + 2 * omegaz * v_y + omegaz**2 * x
-     a_y = -(gamma * Mr * y)/(x**2 + y**2 + z**2)**(1.5) & 
-           -(gamma * Ms * (y - d))/(x**2 + (y - d)**2 + z**2)**(1.5) &
-           - 2 * omegaz * v_x + omegaz**2 * (y - d)
-     a_z = -(gamma * Mr * z)/(x**2 + y**2 + z**2)**(1.5) &
-           -(gamma * Ms * z)/(x**2 + (y - d)**2 + z**2)**(1.5)
+     dt = schrittzaehler * 1.0 
+     write(*,*) dt
 
 
 
-     v_xmid =  v_x + a_x * dt/2 !mittlere Geschw. 
-     v_ymid =  v_y + a_y * dt/2
-     v_zmid =  v_z + a_z * dt/2
-
-
-     x_mid = x +  v_x * dt/2    !mittlere Ort
-     y_mid = y +  v_y * dt/2
-     z_mid = z +  v_z * dt/2
-
-
-     a_xmid = -(gamma * Mr * x_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) &        !mittlere Beschl. 
-              -(gamma * Ms * x_mid)/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5) & 
-              + 2 * omegaz * v_ymid + omegaz**2 * x_mid
-
-     a_ymid = -(gamma * Mr * y_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) & 
-              -(gamma * Ms * (y_mid - d))/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5) &
-              - 2 * omegaz * v_xmid + omegaz**2 * (y_mid - d)
-
-     a_zmid = -(gamma * Mr * z_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) &
-              -(gamma * Ms * z_mid)/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5)
-
-
-     v_xneu = v_x + a_xmid * dt !neue Geschw.
-     v_yneu = v_y + a_ymid * dt
-     v_zneu = v_z + a_zmid * dt
-
-
-     x_neu = x + v_xmid * dt    !neue Koordinaten
-     y_neu = y + v_ymid * dt
-     z_neu = z + v_zmid * dt
+     write(*,*) "#######################################################################"
 
 
 
+     Zeitschleife : do i = 0, n                    
+
+
+        a_x = -(gamma * Mr * x)/(x**2 + y**2 + z**2)**(1.5) & 
+             -(gamma * Ms * x)/(x**2 + (y - d)**2 + z**2)**(1.5) & 
+             + 2 * omegaz * v_y + omegaz**2 * x
+        a_y = -(gamma * Mr * y)/(x**2 + y**2 + z**2)**(1.5) & 
+             -(gamma * Ms * (y - d))/(x**2 + (y - d)**2 + z**2)**(1.5) &
+             - 2 * omegaz * v_x + omegaz**2 * (y - d)
+        a_z = -(gamma * Mr * z)/(x**2 + y**2 + z**2)**(1.5) &
+             -(gamma * Ms * z)/(x**2 + (y - d)**2 + z**2)**(1.5)
 
 
 
-     x = x_neu                  !Puffer, aktualisieren des Ortsvektors
-     y = y_neu
-     z = z_neu
+        v_xmid =  v_x + a_x * dt/2 !mittlere Geschw. 
+        v_ymid =  v_y + a_y * dt/2
+        v_zmid =  v_z + a_z * dt/2
 
 
-
-     v_x = v_xneu               !Puffer, aktualisieren des Geschwindigkeitsvektors
-     v_y = v_yneu 
-     v_z = v_zneu
-
-
-     t = i * dt                         
+        x_mid = x +  v_x * dt/2    !mittlere Ort
+        y_mid = y +  v_y * dt/2
+        z_mid = z +  v_z * dt/2
 
 
+        a_xmid = -(gamma * Mr * x_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) &        !mittlere Beschl. 
+             -(gamma * Ms * x_mid)/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5) & 
+             + 2 * omegaz * v_ymid + omegaz**2 * x_mid
 
-     !write(*,*) r(1)/Radius, r(2)/Radius           !Zuerst x-Achse, dann die y-Achse 
-     !write(*,*) r(1)/Radius, v(1)* (3600/1000)     !Geschw. vs. Ort
-      write(*,*) t/3600, (y-L1)/Rr                  !Zeit[h], Abweichung [Rhearadien]
- 
+        a_ymid = -(gamma * Mr * y_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) & 
+             -(gamma * Ms * (y_mid - d))/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5) &
+             - 2 * omegaz * v_xmid + omegaz**2 * (y_mid - d)
+
+        a_zmid = -(gamma * Mr * z_mid)/(x_mid**2 + y_mid**2 + z_mid**2)**(1.5) &
+             -(gamma * Ms * z_mid)/(x_mid**2 + (y_mid - d)**2 + z_mid**2)**(1.5)
+
+
+        v_xneu = v_x + a_xmid * dt !neue Geschw.
+        v_yneu = v_y + a_ymid * dt
+        v_zneu = v_z + a_zmid * dt
+
+
+        x_neu = x + v_xmid * dt    !neue Koordinaten
+        y_neu = y + v_ymid * dt
+        z_neu = z + v_zmid * dt
 
 
 
 
-  end do Zeitschleife
+
+
+        x = x_neu                  !Puffer, aktualisieren des Ortsvektors
+        y = y_neu
+        z = z_neu
+
+
+
+        v_x = v_xneu               !Puffer, aktualisieren des Geschwindigkeitsvektors
+        v_y = v_yneu 
+        v_z = v_zneu
+
+
+        t = i * dt                         
+
+
+
+        !write(*,*) r(1)/Radius, r(2)/Radius           !Zuerst x-Achse, dann die y-Achse 
+        !write(*,*) r(1)/Radius, v(1)* (3600/1000)     !Geschw. vs. Ort
+        write(*,*) t/3600, (y-L1)/Rr                  !Zeit[h], Abweichung [Rhearadien]
+
+
+        write(*,523) dt, schrittzaehler, t/3600, W(2)/1000 
+523     format(F10.3, 3X, F10.3, 3X, F10.3, 3X, F10.3)
+
+
+
+     end do Zeitschleife
+
+  end do !Schrittweite
 
 end program SaturnRheaTeilchen
