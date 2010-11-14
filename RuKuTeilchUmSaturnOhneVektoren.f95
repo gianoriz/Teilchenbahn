33fa1e775b7f8dd8ba2bb47a1d867e37d17d7a14
Giano Rizzo <rizzo@moe@geo.uni-koeln.de>
2010-11-14 18:23:32 +0100

    ich habe wieder etwas weitergearbeitet

--- RuKuTeilchUmSaturnOhneVektoren.f95
+++ RuKuTeilchUmSaturnOhneVektoren.f95
@@ -46,8 +46,6 @@ program TeilchenUmSaturnTest
 
 
 
-
-
   !STARTWERTE DES TEILCHENS IM RUHESYSTEM VON RHEA: 
   x   = 0.0           !x  Vertikalachse                                      ![m]    Startpunkt
   y   = Rhearadius    !y  Horizontalachse                                    ![m]    Startpunkt             
@@ -55,20 +53,13 @@ program TeilchenUmSaturnTest
   v_x = 0.0                                                         ![m/s]  Startgeschwindigkeit
   v_y = 0.0                                                         ![m/s]  Startgeschwindigkeit
   v_z = 0.0                                                         ![m/s]  Startgeschwindigkeit 
-!  a_x = -(gamma * Ms * x)/(x**2 + (y - d)**2 + z**2)**(1.5) &
-!       + 2 * omegaz * v_y + omegaz**2 * x
-!  a_y = -(gamma * Ms * (y - d))/(x**2 + (y - d)**2 + z**2)**(1.5) &
-!       - 2 * omegaz * v_x + omegaz**2 * (y - d)
-!  a_z = -(gamma * Ms * z)/(x**2 + (y - d)**2 + z**2)**(1.5)
-
 
   dt   = 1000000.0                                       ![s]
 
-  !Runge-Kutta-Solver:
 
   Zeitschleife : do counter = 0, 1000                                    ![Anzahl der Iterationen]
 
-     a_x = -(gamma * Ms * x)/(x**2 + (y - d)**2 + z**2)**(1.5) &
+1     a_x = -(gamma * Ms * x)/(x**2 + (y - d)**2 + z**2)**(1.5) &
           + 2 * omegaz * v_y + omegaz**2 * x
      a_y = -(gamma * Ms * (y - d))/(x**2 + (y - d)**2 + z**2)**(1.5) &
           - 2 * omegaz * v_x + omegaz**2 * (y - d)
@@ -78,40 +69,40 @@ program TeilchenUmSaturnTest
 
      k11 = dt * v_x
      k12 = dt * v_y
- !    k13 = dt * v_z 
+
      k14 = dt * a_x  
      k15 = dt * a_y
- !    k16 = dt * a_z
+
 
      k21 = dt * (v_x + k14/2.)  
      k22 = dt * (v_y + k15/2.)  
-  !   k23 = dt * (v_z + k16/2.) 
+
      k24 = dt * (a_x)
      k25 = dt * (a_y)
-!     k26 = dt * (a_z) 
+
 
      k31 = dt * (v_x + k24/2.)
      k32 = dt * (v_y + k25/2.) 
- !    k33 = dt * (v_z + k26/2.)
+
      k34 = dt * (a_x)
      k35 = dt * (a_y)
-  !   k36 = dt * (a_z)
+
 
      k41 = dt * (v_x + k34)
      k42 = dt * (v_y + k35)
-  !   k43 = dt * (v_z + k36)
+
      k44 = dt * (a_x) 
      k45 = dt * (a_y)
-  !   k46 = dt * (a_z)
+
 
 
      !###################################################################
+  !Runge-Kutta-Solver:
      x   = x   + k11/6. + k21/3. + k31/3. + k41/6.  
      y   = y   + k12/6. + k22/3. + k32/3. + k42/6.  
-!     z   = z   + k13/6. + k23/3. + k33/3. + k43/6.  
+
      v_x = v_x + k14/6. + k24/3. + k34/3. + k44/6.  
      v_y = v_y + k15/6. + k25/3. + k35/3. + k45/6.    
-!     v_z = v_z + k16/6. + k26/3. + k36/3. + k46/6.           !Algorithmus 
      !###################################################################
 
 
