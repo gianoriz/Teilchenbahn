25bf9a9cdf478c904cac9728a5e70af36784a522
Giano Rizzo <rizzo@moe@geo.uni-koeln.de>
2010-11-19 18:27:33 +0100

    vektoren

--- RKVektor.f95
+++ RKVektor.f95
@@ -3,20 +3,48 @@ module Saturndata
   save
 
   ! Konstanten *****************************
-  integer, parameter :: mem = selected_real_kind(10,12)            ! Variablengroesse
-  real(kind=mem) :: gamma = 6.67428e-11                            ![m³/kg*s²]                 (Wiki)
-  real(kind=mem) :: d =  527040000.0                               ![m]   Entf. Rhea zu Saturn (Wiki) 
-  real(kind=mem) :: MSaturn = 5.685e26                             ![kg]  Masse Saturn         (Wiki)
-  real(kind=mem) :: MRhea = 0.000023166e26                         ![kg]  Masse Rhea 2.3166e21 (berechnet) 
-  real(kind=mem) :: L1                                             !   Lagrangepunkt        (berechnet)
-  real(kind=mem) :: RRhea = 764000.0                               ![m]   Rhearadius           (Wiki) 
-  real(kind=mem) :: RSaturn = 57316000.0                           ![m]   Saturnradius         (Wiki)
-  real(kind=mem) :: omegaz                                         ![1/s] Winkelgeschw.        (berechnet) 
+  double precision  :: gamma = 6.67428e-11                            ![m³/kg*s²]                 (Wiki)
+  double precision  :: d =  527040000.0                               ![m]   Entf. Rhea zu Saturn (Wiki) 
+  double precision  :: MSaturn = 5.685e26                             ![kg]  Masse Saturn         (Wiki)
+  double precision  :: MRhea = 0.000023166e26                         ![kg]  Masse Rhea 2.3166e21 (berechnet) 
+  double precision  :: L1                                             !   Lagrangepunkt        (berechnet)
+  double precision  :: RRhea = 764000.0                               ![m]   Rhearadius           (Wiki) 
+  double precision  :: RSaturn = 57316000.0                           ![m]   Saturnradius         (Wiki)
+  double precision  :: omegaz                                         ![1/s] Winkelgeschw.        (berechnet) 
 
   ! Globale Variablen **********************
-  real(kind=mem) :: dt                          ! Zeitintervall
+  double precision  :: dt                          ! Zeitintervall
+  double precision , dimension(3) :: VecX          ! Ortsvektor   
+  double precision , dimension(3) :: VecV          ! Geschwindigkeitsvektor   
 
 end module Saturndata
+
+module vectors
+  implicit none
+  private
+  public :: cross, scalar
+
+  type vector
+     double precision :: x,y,z
+  end type vector
+
+contains
+  function cross(a, b)
+    TYPE (vector), INTENT (in) :: a, b
+    TYPE (vector) :: cross
+
+    cross%x = a%y * b%z - a%z * b%y
+    cross%y = a%z * b%x - a%x * b%z
+    cross%z = a%x * b%y - a%y * b%x
+  end function cross
+
+  function scalar(a, b)
+    TYPE (vector), INTENT (in) :: a, b
+    double precision :: scalar
+    scalar = (a%x * b%x) + (a%y * b%y) + (a%z * b%z)
+  end function scalar
+end module vectors
+
 
 
 
@@ -26,12 +54,19 @@ end module Saturndata
 
 program TeilchenbahnNeu
   use Saturndata
+  use vectors
   implicit none   
 
-  ! Variablen ******************************
 
-  real(kind=mem), dimension(3) :: VecX          ! Ortsvektor   
-  real(kind=mem), dimension(3) :: VecV          ! Geschwindigkeitsvektor   
+
+  ! Funktionen *****************************
+  double precision :: Betrag3
+  double precision :: Wurzel
+  double precision :: Fc
+  double precision :: Fz
+  double precision :: Fg
+
+  ! Variablen ******************************
 
 
   ! Berechnete Variablen *******************
@@ -41,19 +76,72 @@ program TeilchenbahnNeu
   dt = 42
   call Werteausgeben
 
+
+
 end program TeilchenbahnNeu
+
+! **********************************************************************
+! P R O G R A M     E N D
+! **********************************************************************
+
+double precision  function Betrag3(x,y,z)
+  use Saturndata
+  implicit none
+  double precision , intent(in) :: x,y,z
+  write(*,*) x,y,z
+  Betrag3 = sqrt(x**2 + y**2 + z**2)
+  return
+! TESTFUNKTION:
+! write(*,*) Betrag3(dble(1), dble(1), dble(0))
+end function Betrag3
+
+
+
+!---------------------------------------
+double precision  function Fg(x,y,z)
+  use Saturndata
+  implicit none
+  double precision , intent(in) :: x,y,z
+
+
+  return
+end function Fg
+!---------------------------------------
+
+
+
+!---------------------------------------
+double precision  function Fc(x,y,z)
+  use Saturndata
+  implicit none
+  double precision , intent(in) :: x,y,z
+
+
+  return
+end function Fc
+!---------------------------------------
+
+
+
+!---------------------------------------
+double precision  function Fz(x,y,z)
+  use Saturndata
+  implicit none
+  double precision , intent(in) :: x,y,z
+
+
+  return
+end function Fz
+!---------------------------------------
+
 
 
 subroutine Werteausgeben
   use Saturndata
   implicit none
-
-
   write(*,*) "Hallo Du Eimer", dt
-
   return 
 end subroutine Werteausgeben
-! real(kind=mem) function ()
 
 
 
