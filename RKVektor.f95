ec2308fc2243422000a99b16e4d121b4b0dfdc08
Giano Rizzo <rizzo@moe@geo.uni-koeln.de>
2010-11-19 16:31:13 +0100

    Module klappt prima globale Variabr auch

--- RKVektor.f95
+++ RKVektor.f95
@@ -1,59 +1,62 @@
+module Saturndata
+  implicit none
+  save
+
+  ! Konstanten *****************************
+  integer, parameter :: mem = selected_real_kind(10,12)            ! Variablengroesse
+  real(kind=mem) :: gamma = 6.67428e-11                            ![m³/kg*s²]                 (Wiki)
+  real(kind=mem) :: d =  527040000.0                               ![m]   Entf. Rhea zu Saturn (Wiki) 
+  real(kind=mem) :: MSaturn = 5.685e26                             ![kg]  Masse Saturn         (Wiki)
+  real(kind=mem) :: MRhea = 0.000023166e26                         ![kg]  Masse Rhea 2.3166e21 (berechnet) 
+  real(kind=mem) :: L1                                             !   Lagrangepunkt        (berechnet)
+  real(kind=mem) :: RRhea = 764000.0                               ![m]   Rhearadius           (Wiki) 
+  real(kind=mem) :: RSaturn = 57316000.0                           ![m]   Saturnradius         (Wiki)
+  real(kind=mem) :: omegaz                                         ![1/s] Winkelgeschw.        (berechnet) 
+
+  ! Globale Variablen **********************
+  real(kind=mem) :: dt                          ! Zeitintervall
+
+end module Saturndata
+
+
+
+! **********************************************************************
+! P R O G R A M
+! **********************************************************************
+
 program TeilchenbahnNeu
+  use Saturndata
   implicit none   
 
-  !PROGRAMM: RUNGE-KUTTA-4TER-ORDNUNG (MIT DEN RICHTIGEN BEWEGUNGSGLEICHUNGEN)
-  ! Der Letzte Anlauf
+  ! Variablen ******************************
+
+  real(kind=mem), dimension(3) :: VecX          ! Ortsvektor   
+  real(kind=mem), dimension(3) :: VecV          ! Geschwindigkeitsvektor   
 
 
-  !DEFINITION DER VARIABLEN:
-  integer, parameter :: mem = selected_real_kind(10,12)    ! Variablengroesse
+  ! Berechnete Variablen *******************
+  L1 = d - (d/( sqrt(MRhea/MSaturn)+1.0)) 
+  omegaz = sqrt((gamma * MSaturn)/(d**3))
 
-
-
-
-
- !SETZE KONSTANTEN & PARAMETER:
-
-   real(kind=mem) :: gamma = 6.67428e-11                            ![m³/kg*s²]                 (Wiki)
-   real(kind=mem) :: d =  527040000.0                                ![m]   Entf. Rhea zu Saturn (Wiki) 
-   real(kind=mem) :: Ms = 5.685e26                                  ![kg]  Masse Saturn         (Wiki)
-   real(kind=mem) :: Mr = 0.000023166e26                            ![kg]  Masse Rhea 2.3166e21 (berechnet) 
-   real(kind=mem) :: L1 = d - (d/( sqrt(Mr/Ms)+1.0))                ![m]   Lagrangepunkt        (berechnet)
-   real(kind=mem) :: Rr = 764000.0                                  ![m]   Rhearadius           (Wiki) 
-   real(kind=mem) :: Rs = (120536000.0 + 108728000.0)/4             ![m]   Saturnradius         (Wiki)
-   real(kind=mem) :: omegaz = sqrt((gamma * Ms)/(d**3))             ![1/s] Winkelgeschw.        (berechnet) 
-
-
-
-
-
-!!$  real(kind=mem) ::  dt                            !Flugzeit & Zeitintervall
-!!$  real(kind=mem) ::  omegaz                        !Winkelgeschw. in z-Richtung 
-!!$  real(kind=mem) ::  gamma                         !Gravitationskonstante       
-!!$  real(kind=mem) ::  Ms, Mr                        !Masse Saturn, Masse Rhea    
-!!$  real(kind=mem) :: , dimension(9) :: W            !W(i)=(W(1)=x,...,W(4)=v_x,...,W(7)=a_x) Vektor   
-!!$  real(kind=mem) :: , dimension(4,6) :: K          !RungeKuttaKonst.
-!!$  real(kind=mem) ::  d, L1                         !Abstand Saturn-Rhea;L1,L2=Lagrangepunkte
-!!$  real(kind=mem) ::  Rs                            !Saturnradius
-!!$  real(kind=mem) ::  Rr                            !Rhearadius              
-!!$  integer(kind=8) :: i, t                        !Laufvariablen
-!!$  real(kind=mem) ::  Zeit
-
-  !#####################################
-  !Koordinatenursprung liegt bei Rhea
-  !#####################################
-
-
- 
-
-
-
-
-
-
+  dt = 42
+  call Werteausgeben
 
 end program TeilchenbahnNeu
 
 
+subroutine Werteausgeben
+  use Saturndata
+  implicit none
 
 
+  write(*,*) "Hallo Du Eimer", dt
+
+  return 
+end subroutine Werteausgeben
+! real(kind=mem) function ()
+
+
+
+! Wissen-Konserve:
+! COMMON http://www.obliquity.com/computer/fortran/common.html
+! MODULE https://srv.rz.uni-bayreuth.de/lehre/fortran90/vorlesung/V08/V08.html
